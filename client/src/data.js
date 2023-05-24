4c3e49bc78f9863f1a98d9a65e27c850f543e6b2
Jamal Aldin <jamalaldin@MacBook-Pro.local>
2023-05-24 16:57:12 +0300

    Animal Shelter Management System

---
+++ client/src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://rb.gy/0hng3",
+      title: "ADOPT, DON'T SHOP at the HOPE Dog Shelter",
+      desc: "YOUR NEW FRIEND MIGHT JUST BE ONE CLICK AWAY!",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://rb.gy/dahzc",
+      title: "Adopt Don't Shop at the HOPE Dog Shelter",
+      desc: "YOUR NEW FRIEND MIGHT JUST BE ONE CLICK AWAY!",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://rb.gy/wuach",
+      title: "Adopt Don't Shop at the HOPE Dog Shelter",
+      desc: "YOUR NEW FRIEND MIGHT JUST BE ONE CLICK AWAY!",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://rb.gy/kxq34",
+      title: "DOGS",
+      cat:"dogs"
+    },
+    {
+      id: 2,
+      img: "https://rb.gy/8vdf8",
+      title: "CATS",
+      cat:"cats"
+    },
+    {
+      id: 3,
+      img: "https://c4.wallpaperflare.com/wallpaper/255/1015/759/chick-chicken-bird-bunny-rabbit-hd-wallpaper-preview.jpg",
+      title: "OTHERS",
+      cat:"others"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
